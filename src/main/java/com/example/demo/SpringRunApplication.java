5eec115252bd1e5338a39a4847ebecde6d55b44a
puritykid <18310507278@163.com>
2019-07-05 10:10:35 +0800

    修改

--- src/main/java/com/example/demo/SpringRunApplication.java
+++ src/main/java/com/example/demo/SpringRunApplication.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
@@ -17,18 +16,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan("com.example.demo.mapper")
-public class SpringbootdemoApplication extends SpringBootServletInitializer implements CommandLineRunner {
+public class SpringRunApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(SpringbootdemoApplication.class, args);
-//		SpringContextUtil.setApplicationContext(applicationContext);
+		SpringApplication.run(SpringRunApplication.class, args);
 		System.out.println("启动成功！");
 	}
 
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringbootdemoApplication.class);
+		return application.sources(SpringRunApplication.class);
 	}
 
 	
